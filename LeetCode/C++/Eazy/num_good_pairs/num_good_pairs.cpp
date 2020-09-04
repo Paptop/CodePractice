cc92c278aeda8fb45d90310a081e32b8d09b1fcf
ijurcenko <iljajurchenko@email.com>
2020-09-04 13:03:17 +0300

    1512: num of good pairs added quick solution

--- LeetCode/C++/Eazy/num_good_pairs/num_good_pairs.cpp
+++ LeetCode/C++/Eazy/num_good_pairs/num_good_pairs.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
+
+//Brute force
 int numIndenticalPairs(std::vector<int>& nums)
 {
     int res = 0;
@@ -15,10 +18,44 @@ int numIndenticalPairs(std::vector<int>& nums)
     return res;
 }
 
+//Knowing basic combinations 
+//
+// n * (n - 1) / 2 
+//
+// Finding the number of equal elements N
+// Find the number of unique pairs using formula C(k,n) where
+
+int num_indentical_pairs(std::vector<int>& nums)
+{    
+    std::unordered_map<int,int> pairs;
+    int res = 0;
+
+    for(int i = 0; i < nums.size(); ++i)
+    {
+        const int num = nums[i];
+
+        if(pairs.find(num) == pairs.end())
+        {
+            pairs[num] = 1;
+        }
+        else
+        {
+            ++pairs[num];
+        }
+    }
+
+    for(const auto& values : pairs)
+    {
+        res += values.second * (values.second - 1) / 2;
+    }
+
+    return res;
+}
+
 int main()
 {
     std::vector<int> nums = {1, 1, 1, 1}; 
-    std::cout << numIndenticalPairs(nums) << std::endl;
+    std::cout << num_indentical_pairs(nums) << std::endl;
 
     return 0;
 }
